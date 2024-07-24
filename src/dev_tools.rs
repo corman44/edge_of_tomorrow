fa4798b8d543c3622268de1b98846d4e2bab3d29
Cory Johnson <cory1993johnson@gmail.com>
2024-07-24 12:16:48 +0200

    egui build bugfix

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,6 +1,7 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
 use bevy::{dev_tools::states::log_transitions, prelude::*};
+#[cfg(not(target = "wasm"))]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::screen::Screen;
@@ -8,5 +9,6 @@ use crate::screen::Screen;
 pub(super) fn plugin(app: &mut App) {
     // Print state transitions in dev builds
     app.add_systems(Update, log_transitions::<Screen>);
+    #[cfg(not(target = "wasm"))]
     app.add_plugins(WorldInspectorPlugin::new());
 }
