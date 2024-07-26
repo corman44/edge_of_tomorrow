4fa5ac52e4b57a0f1a48b66cc07f4a41256b4a02
cj <cory1993johnson@gmail.com>
2024-07-26 22:24:39 +0200

    here goes nothing

--- src/game/moneys.rs
+++ src/game/moneys.rs
@@ -1,25 +1,28 @@
 use bevy::prelude::*;
 
-use crate::{dev_tools::DevCyclicTimer, screen::Screen};
+use crate::screen::Screen;
 
 use super::spawn::level::PlayerMoneyText;
 
 pub(super) fn plugin(app: &mut App) {
     app.init_resource::<PlayerMoney>();
-    app.add_systems(Update, (grow_money,update_player_money_text).run_if(in_state(Screen::Playing)));
+    app.add_systems(Update, (
+        // grow_money,
+        update_player_money_text
+    ).run_if(in_state(Screen::Playing)));
 }
 
 #[derive(Default, Resource, Debug)]
 pub struct PlayerMoney(pub i32);
 
-fn grow_money(
-    dev_timer: ResMut<DevCyclicTimer>,
-    mut money_res: ResMut<PlayerMoney>,
-) {
-    if dev_timer.timer.finished() {
-        money_res.0 += 5;
-    }
-}
+// fn grow_money(
+//     dev_timer: ResMut<DevCyclicTimer>,
+//     mut money_res: ResMut<PlayerMoney>,
+// ) {
+//     if dev_timer.timer.finished() {
+//         money_res.0 += 5;
+//     }
+// }
 
 fn update_player_money_text(
     mut player_money_text_query: Query<&mut Text, With<PlayerMoneyText>>,
